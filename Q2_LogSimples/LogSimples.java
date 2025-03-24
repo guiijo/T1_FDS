635621733ea3d65af00e01cfd843a2df9c3d6959
Eduardo S. Barbosa <91751876+EduardoSBarbosa@users.noreply.github.com>
2025-03-24 18:41:48 -0300

    Create LogSimples.java(Q2)

---
+++ Q2_LogSimples/LogSimples.java
@@ -0,0 +1,30 @@
+import java.time.LocalDate;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LogSimples implements Iterable<String> {
+    private static LogSimples instancia;
+    private List<String> mensagens;
+
+    private LogSimples() {
+        mensagens = new LinkedList<>();
+    }
+
+    public static LogSimples getInstancia() {
+        if (instancia == null) {
+            instancia = new LogSimples();
+        }
+        return instancia;
+    }
+
+    public void log(String m) {
+        String logM = LocalDate.now().toString() + " : " + m;
+        mensagens.add(logM);
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return mensagens.iterator();
+    }
+}
